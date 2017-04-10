16e7e02127e7016a9f1bb51852595bc82a0b4cfa
Jason Merrill <jason.merrill@stgconsulting.com>
2017-04-10 15:55:42 -0600

    added the view artist page, separated some styles, tweaked eslint

--- source/components/view-artist/view-artist-controller.js
+++ source/components/view-artist/view-artist-controller.js
@@ -1,3 +1,14 @@
-export default function () {
+export default function ($http, $routeParams) {
     'ngInject';
+    var varc = this;
+
+    $http.get('/api/artist.php?artist_id='+$routeParams.id).
+    then(function(response) {
+        varc.artist = response.data;
+    });
+
+	$http.get('/api/album.php?artist_id='+$routeParams.id).
+	then(function(response) {
+		varc.albums = response.data;
+	});
 }
